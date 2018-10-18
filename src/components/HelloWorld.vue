c10102faa698fd1877f9bb349703a6eeb93bf13e
YiYeLiu <790134161@qq.com>
2018-10-18 16:05:45 +0800

    实现路由点击跳转相关页面

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,91 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
-    <mt-button type="default" @click="btnone">default</mt-button>
-    <mt-button type="primary" @click="btntwo">primary</mt-button>
-    <mt-button type="danger">danger</mt-button>
-    <div class="mui-btn mui-btn-primary">
-        蓝色
-    </div>
+    <h1>123</h1>
   </div>
 </template>
 
@@ -99,20 +14,7 @@ export default {
     };
   },
   methods: {
-    btnone() {
-      // Toast('提示信息')
-      Toast({
-        message: "提示",
-        position: "bottom",
-        duration: 5000
-      })
-    },
-    btntwo() {
-      Indicator.open({
-        text: '加载中...',
-        spinnerType: 'fading-circle'
-     });
-    }
+    
   }
 };
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-import homepage from '@/components/homepage'
-
-import { Button } from 'mint-ui'
-Vue.component(Button.name, Button)
+// import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
+/* eslint-disable */
+import HomeContainer from '@/components/HomeContainer'
+import MemberContainer from '@/components/MemberContainer'
+import ShopContainer from '@/components/ShopContainer'
+import SearchContainer from '@/components/SearchContainer'
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
-      path: '/',
-      name: 'homepage',
-      component: homepage
-    }
-  ]
+    { path: '/Home', component: HomeContainer },
+    { path: '/Member', component: MemberContainer },
+    { path: '/Shopcart', component: ShopContainer },
+    { path: '/Search', component: SearchContainer },
+    { path: '*', redirect: '/home'}
+  ],
+  linkActiveClass: 'mui-active'
 })
